1ae1b28f195596f27b9a963222bf4a26aaf4694f
itachixbot65 <botitachi226@gmail.com>
2025-03-16 12:08:32 +0600

    Update convert-sticker.js

--- plugins/convert-sticker.js
+++ plugins/convert-sticker.js
@@ -275,7 +275,7 @@ cmd(
     async (conn, mek, m, { quoted, args, q, reply, from }) => {
         if (!mek.quoted) return reply(`*Reply to any Image or Video, Sir.*`);
         let mime = mek.quoted.mtype;
-        let pack = Config.STICKER_NAME || "Mr Frank OFC";
+        let pack = Config.STICKER_NAME || "Mr Hussain OFC";
         
         if (mime === "imageMessage" || mime === "stickerMessage") {
             let media = await mek.quoted.download();
